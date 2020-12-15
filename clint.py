6ace6f94b9a0391eb5bfafd88c01590e41f7ad17
vikasreddy636 <73032093+vikasreddy636@users.noreply.github.com>
2020-12-15 15:44:05 +0100

    Update clint.py

--- clint.py
+++ clint.py
@@ -1,5 +1,14 @@
+"""
+In this program the clint is requested to enter the required letter for moving of the crane
+"""
 import socket
+"""
+IPV4 (OR) IPV6 can be selected in the below brackets
+"""
 c = socket.socket()
+"""
+IP aaddress of the server must be enterd here and port number can be choosen. Preferred 9999 because of open port
+"""
 c.connect(('127.0.0.1',9999))
 print ("Enter ""A"" to increase by 40\n")
 print ("Enter ""B"" to decrease by 40\n")
