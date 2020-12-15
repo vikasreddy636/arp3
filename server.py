12393f3301d38d47ac192b65ec47ba3f336139bd
vikasreddy636 <73032093+vikasreddy636@users.noreply.github.com>
2020-12-15 15:53:33 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
+"""
+This acts as a server in different machine but shouls be connected to the internet. IPV4 and IPV6 can be entered in the socket. This socket is a bidirectional socket which transfers data from the clint to the server and vice versa
+"""
 import socket
 s = socket.socket()
 print('server socket created')
-
+"""
+IP numbers should be given to the clint sothat they can connect to this server.If both server and clints are in the same network then th ip address can be replaced by 'local host'
+"""
 s.bind(('127.0.0.1',9999))
-
+"""
+No of clients can be decided here
+"""
 s.listen(1)
 print('waiting')
 
 val=0
-
+"""
+Algorithm for the movement of the crane to move forward,backowrds and to stop.
+"""
 while True:
 	c, addr = s.accept()
 	name = c.recv(1024).decode()
